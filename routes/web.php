612cf085f00820904f1b0bc10bbaceaf5a0652a9
verlyra <verlyaulia26@gmail.com>
2026-03-17 13:50:44 +0700

    fix: add comment to clarify view file naming in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('portfolio');
+    return view('portfolio'); // Pastikan nama file di resources/views adalah portfolio.blade.php (huruf kecil semua)
 });
